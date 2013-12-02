855ce94b1c4f6111942fc490ce432a493e784b66
xyiyox <yiyo1224@hotmail.com>
2013-12-02 15:52:21 -0500

    Configuracion inicial de conexion a la base de datos oracle

--- thesis/settings.py
+++ thesis/settings.py
@@ -57,8 +57,13 @@ WSGI_APPLICATION = 'thesis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'xe',
+        'USER': 'pythonhol',
+        'PASSWORD': 'welcome',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
