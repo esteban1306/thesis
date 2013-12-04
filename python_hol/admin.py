5dc23e631128fb91c402de5a16fe54d841fa0c23
xyiyox <yiyo1224@hotmail.com>
2013-12-04 10:58:49 -0500

    Agregados cambios en el crud para la vista del admin

--- python_hol/admin.py
+++ python_hol/admin.py
@@ -1,4 +1,11 @@
 from models import Employees, Jobs
 from django.contrib import admin
-admin.site.register(Employees)
-admin.site.register(Jobs)
+
+class EmployeeAdmin(admin.ModelAdmin):
+	list_display = ('first_name', 'last_name', 'email', 'phone_number', 'hire_date', 'salary')
+	list_filter = ['hire_date']
+	search_fields = ['last_name']
+	date_hierarchy = 'hire_date'
+
+	admin.site.register(Employees)
+	admin.site.register(Jobs)
