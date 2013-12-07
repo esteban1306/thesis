89057afc01acf1aabe764ae6e2282d9b4f0db28d
xyiyox <yiyo1224@hotmail.com>
2013-12-07 15:33:21 -0500

    Agregada la url para redirigir despues de agregar un nuevo tipo de docente

--- thesis/urls.py
+++ thesis/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','principal.views.nuevo_Tipodocente'),
+    url(r'^Tipodocente','principal.views.lista_estudiantes'),
 )
